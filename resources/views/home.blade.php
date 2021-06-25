504baf5747065f14ce4625d3274df8b3cb0df6ed
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-06-25 15:30:26 +0100

    did the first page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid p-0 m-0">
+        <div class="isHero">
+            <div class="darkOverlay">
+                <div class="isNavbar sticky-top">
+                    <div class="container py-3">
+                        <div class="row">
+                            <div class="col-md-7 col-xl-4">
+                                <svg class="open-sign" id="open" xmlns="http://www.w3.org/2000/svg" width="86" height="86" viewBox="0 0 86 86">
+                                        <g id="Group_1" data-name="Group 1" transform="translate(16.192 8.096)">
+                                            <path id="Path_1" data-name="Path 1" d="M18.71,43.557a2.52,2.52,0,0,1-2.222-3.7L32.68,9.432A2.52,2.52,0,1,1,37.128,11.8L20.936,42.221a2.52,2.52,0,0,1-2.226,1.336Z" transform="translate(-16.192 -8.096)" fill="#f2d231"/>
+                                        </g>
+                                        <g id="Group_2" data-name="Group 2" transform="translate(48.576 8.096)">
+                                            <path id="Path_2" data-name="Path 2" d="M67.29,43.557a2.519,2.519,0,0,1-2.226-1.336L48.872,11.8A2.52,2.52,0,1,1,53.32,9.432L69.512,39.853a2.52,2.52,0,0,1-2.222,3.7Z" transform="translate(-48.576 -8.096)" fill="#e79d26"/>
+                                        </g>
+                                        <g id="Group_12" data-name="Group 12">
+                                            <g id="Group_4" data-name="Group 4" transform="translate(0 38.517)">
+                                                <g id="Group_3" data-name="Group 3">
+                                                    <path id="Path_3" data-name="Path 3" d="M83.48,76.734a6.767,6.767,0,0,1-6.747,6.747H9.266A6.766,6.766,0,0,1,2.52,76.734V47.784a6.766,6.766,0,0,1,6.746-6.747H76.733a6.767,6.767,0,0,1,6.747,6.747Z" transform="translate(0 -38.517)" fill="#8ee16a"/>
+                                                    <path id="Path_4" data-name="Path 4" d="M76.733,41.037H43V83.48H76.733a6.767,6.767,0,0,0,6.747-6.747V47.784A6.767,6.767,0,0,0,76.733,41.037Z" transform="translate(0 -38.517)" fill="#6ebd6b"/>
+                                                    <path id="Path_5" data-name="Path 5" d="M76.733,86H9.266A9.277,9.277,0,0,1,0,76.734V47.784a9.277,9.277,0,0,1,9.266-9.266H76.733A9.277,9.277,0,0,1,86,47.784v28.95A9.277,9.277,0,0,1,76.733,86ZM9.266,43.556a4.232,4.232,0,0,0-4.227,4.227v28.95a4.232,4.232,0,0,0,4.227,4.227H76.733a4.232,4.232,0,0,0,4.227-4.227V47.784a4.232,4.232,0,0,0-4.227-4.227H9.266Z" transform="translate(0 -38.517)" fill="#faf3f4"/>
+                                                </g>
+                                            </g>
+                                            <path id="Path_6" data-name="Path 6" d="M43,0A10.616,10.616,0,1,0,53.615,10.616,10.628,10.628,0,0,0,43,0Z" transform="translate(0)" fill="#bce9f8"/>
+                                            <g id="Group_11" data-name="Group 11" transform="translate(19.361 54.163)">
+                                                <g id="Group_5" data-name="Group 5" transform="translate(0 0)">
+                                                    <path id="Path_7" data-name="Path 7" d="M19.361,65.323v-6.13c0-3.669,2.241-5.031,5.141-5.031s5.163,1.362,5.163,5.031v6.13c0,3.669-2.263,5.031-5.163,5.031S19.361,68.992,19.361,65.323Zm7.448-6.13c0-1.736-.879-2.526-2.307-2.526-1.45,0-2.285.791-2.285,2.526v6.13c0,1.736.835,2.527,2.285,2.527,1.428,0,2.307-.791,2.307-2.527Z" transform="translate(-19.361 -54.163)" fill="#faf3f4"/>
+                                                </g>
+                                                <g id="Group_6" data-name="Group 6" transform="translate(12.741 0)">
+                                                    <path id="Path_8" data-name="Path 8" d="M32.1,55.041a.969.969,0,0,1,1.077-.879H37.6c2.79,0,4.987,1.34,4.987,4.944v.132c0,3.6-2.264,4.987-5.163,4.987h-2.46v5.1c0,.593-.725.9-1.428.9-.725,0-1.429-.308-1.429-.9Zm2.856,1.626v5.317h2.46c1.428,0,2.307-.813,2.307-2.526v-.264c0-1.713-.879-2.526-2.307-2.526Z" transform="translate(-32.103 -54.163)" fill="#faf3f4"/>
+                                                </g>
+                                                <g id="Group_8" data-name="Group 8" transform="translate(24.979)">
+                                                    <g id="Group_7" data-name="Group 7">
+                                                        <path id="Path_9" data-name="Path 9" d="M47.2,61.061h3.339c.571,0,.9.549.9,1.143a1,1,0,0,1-.9,1.1H47.2v4.416h6.218c.571,0,.9.593.9,1.274,0,.593-.286,1.231-.9,1.231H45.592c-.637,0-1.252-.308-1.252-.9V55.063c0-.593.615-.9,1.252-.9h7.822c.615,0,.9.637.9,1.23,0,.681-.33,1.275-.9,1.275H47.2Z" transform="translate(-44.34 -54.163)" fill="#daebeb"/>
+                                                    </g>
+                                                </g>
+                                                <g id="Group_10" data-name="Group 10" transform="translate(36.622 0)">
+                                                    <g id="Group_9" data-name="Group 9">
+                                                        <path id="Path_10" data-name="Path 10" d="M63.563,69.322,58.839,60.49v8.832c0,.593-.725.9-1.428.9-.725,0-1.428-.308-1.428-.9V55.063c0-.615.7-.9,1.428-.9,1.033,0,1.451.22,2.132,1.516l4.24,8.2V55.041c0-.615.7-.879,1.428-.879.7,0,1.428.264,1.428.879V69.322c0,.593-.725.9-1.428.9a1.716,1.716,0,0,1-1.647-.9Z" transform="translate(-55.983 -54.163)" fill="#daebeb"/>
+                                                    </g>
+                                                </g>
+                                            </g>
+                                        </g>
+                                        <path id="Path_11" data-name="Path 11" d="M76.733,38.517H43v5.039H76.733a4.232,4.232,0,0,1,4.227,4.227v28.95a4.232,4.232,0,0,1-4.227,4.227H43V86H76.733A9.277,9.277,0,0,0,86,76.734V47.784A9.277,9.277,0,0,0,76.733,38.517Z" transform="translate(0)" fill="#daebeb"/>
+                                        <path id="Path_12" data-name="Path 12" d="M53.615,10.616A10.628,10.628,0,0,0,43,0V21.231A10.627,10.627,0,0,0,53.615,10.616Z" transform="translate(0)" fill="#9ce0f6"/>
+                                    </svg>
+                                <button class="header-phone">07305 436 556 <br>
+                                    <span style="color:#1DD600">Free delivery</span>
+                                </button>
+                            </div>
+                            <div class="d-sm-none d-md-none d-xl-flex col-xl-6 menu">
+                                <button class="x-button">Home</button>
+                                <button class="x-button  x-special">Our Menu</button>
+                                <button class="x-button ">F.A.Q</button>
+                                <button class="x-button ">Contact</button>
+                            </div>
+                            <div class="col-md-3 d-inline-flex align-items-center justify-content-center col-xl-2 cart">
+                                @include('shared.cart')
+                            </div>
+                            <div class=" col-md-2 d-md-flex d-xl-none burger-menu-button">
+                                <button class="x-button w-100" style="font-size:30px;"><i class="bi bi-three-dots-vertical"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="heroText w-100 text-center" >
+                        <h1 class="beastText">
+                           <span class="x-1"> THE </span><br>
+                           <span class="x-2"> BEAST </span><br>
+                           <span class="x-3"> BURGERS </span><br>
+                            <span class="x-5">IN</span> <span class="x-4">  TOWN </span><br>
+                        </h1>
+                </div>
+                <div class="call-to-action">
+                    <button class="call-to-action-btn">ORDER NOW</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/shared/cart.blade.php
@@ -0,0 +1,8 @@
+<div class="x-cart">
+    <i class="bi bi-cart4"></i>
+     :  2
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    £19.20
+    <span class="visually-hidden">unread messages</span>
+  </span>
+</div>
