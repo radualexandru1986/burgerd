8f0baab81fd4956eb8ef07dc5a1f029c4d2b384c
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-06-29 10:46:43 +0100

    Coded the item card ..

--- resources/views/shared/item-card.blade.php
+++ resources/views/shared/item-card.blade.php
@@ -10,22 +10,26 @@
                 <i class="bi bi-star-fill"></i>
             </div>
             <div class="col-5 d-flex justify-content-end" >
-                <span style="text-align: right">8.99</span>
+                <span class="item-card-price" style="text-align: right">£ 8.99</span>
             </div>
         </div>
         <h4 class="card-title">Card title</h4>
         <p class="card-text" style="font-weight: lighter">
-            Mayo and chicken for your delight.
+            A huge single or triple burger with all the fixings, cheese, lettuce, tomato, onions and special sauce or mayonnaise!
         </p>
-        <div class="row mt-5 mb-2">
+        <a class="btn btn-light view-recipe-button">
+            <span class="view-recipe-button-text">View Recipe</span>
+        </a>
+        <div class="row mt-5 mb-2 add-area">
             <div class="col-6 mx-auto">
-                <h2 class="w-100 text-center"> <i class="bi bi-plus-circle-fill"></i> 1 <i class="bi bi-dash-circle-fill"></i> </h2>
+                <h2 class="w-100 text-center"> <i class="bi bi-plus-circle-fill add-button mx-2"></i> <strong class="font-size-40">1</strong> <i class="bi bi-dash-circle-fill mx-2 add-button"></i> </h2>
             </div>
         </div>
+
         <div class="row">
             <div class="col-12 text-center my-2">
-               <button class="x-button">
-                   Add to cart
+               <button class="x-button w-100">
+                  <span class="mr-5">Add to Cart</span>  <i class="bi bi-cart4 ml-5"></i>
                </button>
             </div>
         </div>
