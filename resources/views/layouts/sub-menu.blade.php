694982603cd33b872a1387082e2de48a45f7e05d
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-07-05 20:04:36 +0100

    Added repaired the menu

--- resources/views/layouts/sub-menu.blade.php
+++ resources/views/layouts/sub-menu.blade.php
@@ -1,14 +1,23 @@
-<div class="sub-menu mx-auto d-flex justify-content-between text-center col-lg-12 col-sm-10">
-    <div class="x-button">
-        Burgers
-    </div>
-    <div class="x-button">
-        Sides
-    </div>
-    <div class="x-button">
-        Beverages
-    </div>
-    <div class="x-button">
-        Sweets
+<div class="sub-menu w-100">
+    <div class="row bg-dark w-100 mx-auto d-flex flex-row">
+       <div class="x-button col">
+           Burgers
+       </div>
+       <div class="x-button col">
+           Salad
+       </div>
+        <div class="x-button col">
+           Sides
+       </div>
+        <div class="x-button col">
+           Vegan
+       </div>
+        <div class="x-button col">
+           Beverages
+       </div>
+        <div class="x-button col">
+           Desserts
+       </div>
+
     </div>
 </div>
--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,3 +1,6 @@
-<div class="w-100 m-0 p5 bg-dark text-light text-center">
-    <span class="display-1">This is the footer</span>
-</div>
+<footer class="w-100 m-0 p5 bg-dark text-light text-center d-flex flex-column align-items-center justify-content-center">
+    <div class="col-3  mx-auto">
+        <h5>Made by</h5>
+        <h4>NorDev</h4>
+    </div>
+</footer>
