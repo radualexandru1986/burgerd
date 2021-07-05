80ad55b0ffa0f25114707224f665a5199e957b54
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-07-05 15:12:48 +0100

    The cards are nice centered on some other tablets

--- resources/views/layouts/menu-section.blade.php
+++ resources/views/layouts/menu-section.blade.php
@@ -1,20 +1,18 @@
 @include('shared.divider')
 <div class="menu-items-container" >
-    <div class="row mt-4">
-        <div class="col-xxl-4 col-md-6 col-sm-12 my-3">
+    <div class="row mt-4 justify-content-center">
+        <div class="col-xxl-4 col-md-6 col-sm-10 mx-sm-auto my-3">
             @include('shared.item-card')
         </div>
-        <div class="col-xxl-4 col-md-6 col-sm-12 my-3">
+        <div class="col-xxl-4 col-md-6 col-sm-10 mx-sm-auto my-3">
             @include('shared.item-card')
         </div>
-        <div class="col-xxl-4 col-md-6 col-sm-12 my-3">
+        <div class="col-xxl-4 col-md-6 col-sm-10 mx-sm-auto my-3">
             @include('shared.item-card')
         </div>
-        <div class="col-xxl-4 col-md-6 col-sm-12 my-3">
+        <div class="col-xxl-4 col-md-6 col-sm-10 mx-sm-auto my-3">
             @include('shared.item-card')
         </div>
-        <div class="col-xxl-4 col-md-6 col-sm-12 my-3">
-            @include('shared.item-card')
-        </div>
+
     </div>
 </div>
