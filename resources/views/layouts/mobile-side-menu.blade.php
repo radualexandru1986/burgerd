706f808c3138cbe023110de6e4619807bdd140b2
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-07-14 19:21:03 +0100

    sdsd

--- resources/views/layouts/mobile-side-menu.blade.php
+++ resources/views/layouts/mobile-side-menu.blade.php
@@ -2,10 +2,10 @@
     <div class="close-button text-center text-light" style="font-size: 50px;">
         <i class="bi bi-x-square" onclick="toggleMenuState()"></i>
         <div class="mobileMenuButtonsContainer">
-            <button class="btn btn-light w-100 py-4" onclick="window.location.href='/'">Home</button>
-            <button class="btn btn-light w-100 py-4" onclick="window.location.href='/order'">Order</button>
-            <button class="btn btn-light w-100 py-4" onclick="window.location.href='/faq'" >F.A.Q</button>
-            <button class="btn btn-light w-100 py-4" onclick="window.location.href='/contact'">Contact</button>
+            <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/'">Home</button>
+            <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/order'">Order</button>
+            <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/faq'" >F.A.Q</button>
+            <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/contact'">Contact</button>
         </div>
     </div>
 </div>
