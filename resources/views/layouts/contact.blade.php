7dd5aa71693d5bbbd49e8c8f4261581f789008e5
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-07-15 19:22:34 +0100

    debugging

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -4,10 +4,10 @@
         <div class="container-xxl d-flex justify-content-center align-items-center bg-light has-form" style="min-height: 75vh;">
             <div class="col-10 mx-auto py-5">
                 <div class="row ">
-                    <div class="col-12 col-lg-5 mx-auto mb-5 mb-md-1">
+                    <div class="col-12 col-xl-5 mx-auto mb-5 mb-md-1">
                         <h1 class="display-4">Fill the form</h1>
                         <h1>It's easy..</h1>
-                        <form class="row g-3 mt-5">
+                        <form class="row g-3 mt-5 ">
                             <div class="col-12">
                                 <label for="inputZip" class="form-label">Name</label>
                                 <input type="text" class="form-control" id="inputZip">
@@ -28,7 +28,7 @@
                             </div>
                         </form>
                     </div>
-                    <div class="col-12 col-lg-6 right-column" >
+                    <div class="col-12 col-xl-6 right-column" >
                         <h1 class="display-4">or you can use </h1>
                         <h1>..other ways</h1>
                         <div class="block-container mt-4">
