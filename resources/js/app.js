4254b73a597df6d32ae423342daeefa4ff6cfa2a
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-07-16 15:28:02 +0100

    added the add to cart and vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,54 @@
 require('./bootstrap');
 
+// window.Vue = require('vue').default;
+import Vue from 'vue';
+import Vuex from 'vuex'
+import {burgerstore} from './store/store'
+import ls, {get, set} from 'local-storage'
+Vue.config.ignoredElements = [
+    'x-item']
+Vue.use(Vuex)
+const store = new Vuex.Store(burgerstore)
+
+Vue.component('add-to-cart-component', require('./components/AddToCartComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    store:store,
+    components :{
+
+    },
+    computed : {
+        getTotal(){
+            let orderItems = this.$store.state.orderItems;
+            if(orderItems.length > 0){
+                return orderItems
+                    .map(item=>Number(item.perItem)*Number(item.quantity))
+                    .reduce((prev, curr) => prev + curr, 0).toFixed(2)
+            }else{
+                return 0
+            }
+        },
+        getItemsTotal() {
+            let orderItems = this.$store.state.orderItems;
+            if(orderItems.length > 0){
+                return orderItems
+                    .map(item=>Number(item.quantity))
+                    .reduce((prev, curr) => prev + curr, 0).toFixed(0)
+            }else{
+                return 0
+            }
+        }
+    },
+    beforeMount() {
+        if(ls.get('orderItems')){
+            this.$store.commit('resurrectOrders', ls.get('orderItems'))
+        }
+    }
+});
--- resources/views/layouts/mobile-side-menu.blade.php
+++ resources/views/layouts/mobile-side-menu.blade.php
@@ -9,38 +9,3 @@
         </div>
     </div>
 </div>
-@push('scripts')
-    <script>
-        function toggleMenuState()
-        {
-            let store = window.localStorage;
-            if(document.querySelector('.side-container').classList.contains('closed') && store.getItem('menuState')==='open') {
-                toggleMenu(store.getItem('menuState'))
-            }
-            if (store.getItem('menuState') ) {
-                if (store.getItem('menuState')==='open'){
-                    store.setItem('menuState', 'closed');
-                }else{
-                    store.setItem('menuState', 'open');
-                }
-                toggleMenu(store.getItem('menuState'))
-            }
-
-            if(!store.getItem('menuState')){
-                store.setItem('menuState', 'open')
-                toggleMenu(store.getItem('menuState'))
-            }
-
-        }
-
-        function toggleMenu(status) {
-            if(status==='closed') {
-                document.querySelector('.side-container').classList.replace('open', 'closed')
-            }
-            if(status==='open') {
-                document.querySelector('.side-container').classList.replace('closed', 'open')
-            }
-        }
-
-    </script>
-@endpush
