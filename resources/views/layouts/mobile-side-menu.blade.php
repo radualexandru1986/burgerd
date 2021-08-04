3960f06ed0b1a077273534bc284183db918c609f
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-08-04 12:49:47 +0100

    deleted the order button from the mobile menu

--- resources/views/layouts/mobile-side-menu.blade.php
+++ resources/views/layouts/mobile-side-menu.blade.php
@@ -3,7 +3,6 @@
         <i class="bi bi-x-square" onclick="toggleMenuState()"></i>
         <div class="mobileMenuButtonsContainer">
             <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/'">Home</button>
-            <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/order'">Order</button>
             <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/faq'" >F.A.Q</button>
             <button class="btn btn-dark w-100 py-4 border-1 border-warning" onclick="window.location.href='/contact'">Contact</button>
         </div>
