f5a0b84d71be4b616cd3ebabbdd4d7af738f3f84
radualexandru1986 <radu.c.alexandru@gmail.com>
2021-08-05 18:05:44 +0100

    debugging

--- app/Notifications/NewOrderNotification.php
+++ app/Notifications/NewOrderNotification.php
@@ -30,7 +30,7 @@ class NewOrderNotification extends Notification implements ShouldQueue
      */
     public function via($notifiable)
     {
-        return ['mail', 'database'];
+        return ['mail'];
     }
 
     /**
